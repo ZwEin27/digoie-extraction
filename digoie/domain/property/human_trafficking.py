feee9ad0aa8903480e5aa6755346014be1feaa82
ZwEin <zwein27@gmail.com>
2016-03-08 13:22:30 -0800

    organize labels

---
+++ digoie/domain/property/human_trafficking.py
@@ -0,0 +1,82 @@
+
+""" Definition
+
+Identification:
+http://www.usccb.org/about/anti-trafficking-program/identifying-trafficking-victims.cfm
+https://polarisproject.org/recognize-signs
+
+Resources:
+http://humantrafficking.unc.edu/resources/
+
+"""
+
+# Common Work and Living Conditions: The individual(s) in question
+
+HT_NO_FREEDOM = 1       # Is not free to leave or come and go as he/she wishes
+HT_UNDER_18 = 2         # Is under 18 and is providing commercial sex acts
+HT_COMMERCIAL_PIMP = 3  # Is in the commercial sex industry and has a pimp / manager
+HT_LOW_PAID = 4         # Is unpaid, paid very little, or paid only through tips
+HT_UNUSUAL_WORKTIME = 5 # Works excessively long and/or unusual hours
+HT_NO_BREAK = 6         # Is not allowed breaks or suffers under unusual restrictions at work
+HT_OWE_DEBT = 7         # Owes a large debt and is unable to pay it off
+HT_FALSE_PROMISE = 8    # Was recruited through false promises concerning the nature and conditions of his/her work
+HT_HIGH_SECURITY_MEASURE = 9 # High security measures exist in the work and/or living locations (e.g. opaque windows, boarded up windows, bars on windows, barbed wire, security cameras, etc.)
+
+# Poor Mental Health or Abnormal Behavior
+"""
+Is fearful, anxious, depressed, submissive, tense, or nervous/paranoid
+Exhibits unusually fearful or anxious behavior after bringing up law enforcement
+Avoids eye contact
+
+"""
+HT_POOR_MHNAB = 10      # Poor Mental Health or Abnormal Behavior
+
+# Lack of Control
+"""
+Has few or no personal possessions
+Is not in control of his/her own money, no financial records, or bank account
+Is not in control of his/her own identification documents (ID or passport)
+Is not allowed or able to speak for themselves (a third party may insist on being present and/or translating)
+"""
+HT_LACK_OF_CONTROL = 20
+
+
+# OTHERS
+"""
+Claims of just visiting and inability to clarify where he/she is staying/address
+Lack of knowledge of whereabouts and/or do not know what city he/she is in
+Loss of sense of time
+Has numerous inconsistencies in his/her story
+"""
+HT_INA_LOCATION = 30
+HT_INA_TIME = 31
+HT_INCONSISTENCY = 32
+
+
+# DESCRIPTION
+# http://adjectivesstarting.com/adjectives-to-describe-a-person/
+
+HT_DESC_NATIONALITY = 110   # Nationality is the nation or country to which someone belongs to. 
+HT_DESC_PHSICAL_APPEARANCE = 120          # Physical appearance is self-explanatory.
+HT_DESC_PERSONALITY = 130   # Personality means an individual’s tendencies to behave, think, and react in a particular way. 
+HT_DESC_STYLE = 140         # A person’s style is really their way of dressing, and the way they carry themselves.
+HT_DESC_CHAR_TRAIT = 150    # Characteristics and Traits
+
+
+# ACTION
+
+HT_ACT_SERVICE_VISIT = 201  # girl visit
+HT_ACT_SERVICE_UNPAID = 202 # unpaid service
+HT_ACT_SERVICE_AFTER = 203  # action after service
+
+HT_ACT_SEX_GROUP = 210      # group sex, more than one man and woman
+HT_ACT_SEX_POSITION = 211
+
+HT_ACT_FIND = 220             # the way to find this
+
+
+
+
+# HT_LABELS = []
+
+
